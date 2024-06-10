4cdab44cfd1257f6f40ebd24e832b7e77c762975
Milad Flores <144237667+Uknownpotato@users.noreply.github.com>
2024-06-10 03:32:07 +0200

    Update Inventory.h

--- Inventory.h
+++ Inventory.h
@@ -1,57 +1,83 @@
-#include <iostream>
-#include <string>
-#include <unordered_map>
+#ifndef INVENTORY_H
+#define INVENTORY_H
 
-using namespace std;
+#include <Arduino.h>
+
+const int MAX_PRODUCTS = 75; // Maximum number of unique products
+const int EAN_LENGTH = 14;    // Length of EAN code (13 digits + null terminator)
 
 class Inventory {
-private:
-    unordered_map<unsigned long, int> inventory;
-    int date[3];
-    // quantity par 
+    char inventory[MAX_PRODUCTS][EAN_LENGTH];   // Array to store EAN codes
+    int inventoryQuantity[MAX_PRODUCTS];        // Array to store quantities
+    int uniqueProducts;                         // Number of unique products
 
-public:    
-    Inventory(int dd, int mm, int yy) : date{ dd, mm, yy } {}
+public:
+    // Constructor    
+    Inventory() : uniqueProducts{ 0 } {}
 
     // Adds product
-    void addProduct(unsigned long obj) {
-
-        // Check if product already exist
-        if (searchInventory(obj)) {
-            // If it exists, increment by one
-            inventory[obj]++;
+    void addProduct(const char* obj) {
+      // Check if product already exist
+      int index = searchInventory(obj);
+        // If product exists, increment by 1
+        if (-1 != index) {
+            inventoryQuantity[index]++;
         }
         else {
-            // If it does not exist, add to inventory with the quantity of 1
-            inventory[obj] = 1;
+            // If product does not exist and amount of unique products is less than maximum amount,
+            // add to inventory with the quantity of 1
+            if(uniqueProducts < MAX_PRODUCTS) {
+              strncpy(inventory[uniqueProducts], obj, EAN_LENGTH);
+              inventory[uniqueProducts][EAN_LENGTH - 1] = '\0';
+              inventoryQuantity[uniqueProducts] = 1;
+              uniqueProducts++;
+            } else {
+              Serial.println("Inventory is full!");
+            }
         }
     }
 
     // Removes product
-    void removeProduct(unsigned long obj) {
-
-        // Check if product exists
-        bool productExists = searchInventory(obj);
-
-        // If the product exists and quantity is greater than 1, decrement te quantity
-        if (productExists && inventory[obj] > 1) {
-            inventory[obj]--;
-        }
-        else if (productExists && inventory[obj] == 1) {
-            // If the product exists and quantity is equal to 1, remove the product
-            inventory.erase(obj);
-        }
-        else {
-            //In this case there is no product with this EAN, what to do? Error message?
-        }
+    void removeProduct(const char* obj) {
+      // Check if product exists
+      int index = searchInventory(obj);
+    
+      if(-1 != index) {
+        // If the quantity is greater than 1, decrement the amount
+        if(inventoryQuantity[index] > 1) {
+          inventoryQuantity[index]--;
+        } else {
+          // If the quantity is 1, remove the product
+          for(int i = index; i < uniqueProducts - 1; i++) {
+            strncpy(inventory[i], inventory[i + 1], EAN_LENGTH);
+            inventoryQuantity[i] = inventoryQuantity[i + 1];
+          }
+          uniqueProducts--;
+        }  
+      } else {
+        Serial.println("Product not  found!");
+      }
+      
     }
 
-    // Looks for a product in the inventory
-    bool searchInventory(unsigned long obj) {
-        return inventory.find(obj) != inventory.end();
+    // Searches for a product in the inventory
+    int searchInventory(const char* obj) {
+        for(int i = 0; i < uniqueProducts; i++) {
+          if(strncmp(inventory[i], obj, EAN_LENGTH) == 0)
+            return i;
+        }
+        return -1;      // Product not found
     }
 
-    void displayProduct(unsigned long obj) {
-
+    void displayInventory() {
+      Serial.println("Inventory:");
+      for(int i = 0; i < uniqueProducts; i++) {
+        Serial.print("EAN: ");
+        Serial.print(inventory[i]);
+        Serial.print(", Quantity: ");
+        Serial.println(inventoryQuantity[i]);
+      }
     }
 };
+
+#endif
