f284ba3ccecbcae4efbf629e0ddd8c731cae8e8f
Milad Flores <144237667+Uknownpotato@users.noreply.github.com>
2024-06-11 03:06:58 +0200

    Update Inventory.h
    New version W/O comments

--- Inventory.h
+++ Inventory.h
@@ -1,83 +1,162 @@
 #ifndef INVENTORY_H
 #define INVENTORY_H
 
-#include <Arduino.h>
+#include <SPI.h>
+#include <SdFat.h>
 
-const int MAX_PRODUCTS = 75; // Maximum number of unique products
+SdFat sd;
+
+extern void resetInactivityTime();
+
+const char* cacheFileName = "cache.txt";
+
 const int EAN_LENGTH = 14;    // Length of EAN code (13 digits + null terminator)
 
 class Inventory {
-    char inventory[MAX_PRODUCTS][EAN_LENGTH];   // Array to store EAN codes
-    int inventoryQuantity[MAX_PRODUCTS];        // Array to store quantities
-    int uniqueProducts;                         // Number of unique products
+  const char* inventoryFileName = "inventory.txt";
+
+  // Add item to inventory
+  void addProduct(const char* eanCode) {
+    File inventoryFile;
+    if(!inventoryFile.open(inventoryFileName, O_WRITE | O_CREAT | O_APPEND)) {
+      Serial.println("Failed to open inventory file!");
+      return;
+    }
+
+    char line[20];
+
+    if(eanExist(eanCode)) {
+      inventoryFile.seekSet(0);
+      while(inventoryFile.available()) {
+        inventoryFile.readBytesUntil('\n', line, sizeof(line));
+        if(strncmp(line, eanCode, EAN_LENGTH) == 0) {
+          int currQty = atoi(&line[EAN_LENGTH + 1]);
+          currQty++;
+          sprintf(line, "%s %d", eanCode, currQty);
+          break;
+        }
+      }
+    } else {
+      Serial.println("Adding new product...");
+      sprintf(line, "%s 1", eanCode);
+    }
+
+    if(inventoryFile.println(line)) {
+      Serial.println("Item added successfully.");
+    } else {
+      Serial.println("Failed to add item!");
+    }
+
+    inventoryFile.close();
+  }
+
+  // Remove item from inventory
+  void removeProduct(const char* eanCode) {
+    if(!eanExist(eanCode)) {
+      Serial.println("Product not found in inventory.");
+      return;
+    }
+
+    File inventoryFile;
+    if(!inventoryFile.open(inventoryFileName, O_READ)) {
+      Serial.println("Failed to open inventory file!");
+      return;
+    }
+
+    File tempFile;
+    if(!tempFile.open("temp.txt", O_WRITE | O_CREAT | O_TRUNC)) {
+      Serial.println("Failed to open temporary file!");
+      return;
+    }
+
+    bool found = false;
+
+    while(inventoryFile.available()) {
+      char line[20];
+      inventoryFile.readBytesUntil('\n', line, sizeof(line));
+
+      if(strncmp(line, eanCode, EAN_LENGTH) == 0) {
+        found = true;
+
+        int currQty = atoi(&line[EAN_LENGTH + 1]);
+
+        if(currQty > 1) {
+          currQty--;
+          sprintf(line, "%s %d", eanCode, currQty);
+          tempFile.println(line);
+        } else {
+
+        }
+      } else {
+        tempFile.println(line);
+      }
+    }
+
+    inventoryFile.close();
+    tempFile.close();
+
+    sd.remove(inventoryFileName);
+    sd.rename("temp.txt", inventoryFileName);
+
+    if(found) {
+      Serial.println("Product removed successfully.");
+    } else {
+      Serial.println("Product not found in inventory.");
+    }
+
+  }
+
+  // Fetch EAN from serial
+  void scanEAN(char* eanCode, int maxLen) {
+    int index = 0;
+    while(Serial.available() == 0) {};
+    while(Serial.available() > 0 && index < maxLen - 1) {
+      eanCode[index] = Serial.read();
+      index++;
+      resetInactivityTime();
+    }
+    eanCode[index] = '\0';
+  }
+
+  // Check if ean has been scanned previously
+  bool eanExist(const char* eanCode) {
+    File inventoryFile;
+    if(!inventoryFile.open(inventoryFileName, O_READ)) {
+      Serial.println("Failed to open inventory file!");
+      return false;
+    }
+
+    bool exist = false;
+    while(inventoryFile.available()) {
+      String line = inventoryFile.readStringUntil('\n');
+      if(line.startsWith(eanCode)) {
+        exist = true;
+        break;
+      }
+    }
+
+    inventoryFile.close();
+    return exist;
+  }
 
 public:
     // Constructor    
-    Inventory() : uniqueProducts{ 0 } {}
+    Inventory() {}
 
     // Adds product
-    void addProduct(const char* obj) {
-      // Check if product already exist
-      int index = searchInventory(obj);
-        // If product exists, increment by 1
-        if (-1 != index) {
-            inventoryQuantity[index]++;
-        }
-        else {
-            // If product does not exist and amount of unique products is less than maximum amount,
-            // add to inventory with the quantity of 1
-            if(uniqueProducts < MAX_PRODUCTS) {
-              strncpy(inventory[uniqueProducts], obj, EAN_LENGTH);
-              inventory[uniqueProducts][EAN_LENGTH - 1] = '\0';
-              inventoryQuantity[uniqueProducts] = 1;
-              uniqueProducts++;
-            } else {
-              Serial.println("Inventory is full!");
-            }
-        }
+    void addProductFromScan() {
+      char eanCode[EAN_LENGTH];
+      scanEAN(eanCode, EAN_LENGTH);
+      addProduct(eanCode);
     }
 
     // Removes product
-    void removeProduct(const char* obj) {
-      // Check if product exists
-      int index = searchInventory(obj);
-    
-      if(-1 != index) {
-        // If the quantity is greater than 1, decrement the amount
-        if(inventoryQuantity[index] > 1) {
-          inventoryQuantity[index]--;
-        } else {
-          // If the quantity is 1, remove the product
-          for(int i = index; i < uniqueProducts - 1; i++) {
-            strncpy(inventory[i], inventory[i + 1], EAN_LENGTH);
-            inventoryQuantity[i] = inventoryQuantity[i + 1];
-          }
-          uniqueProducts--;
-        }  
-      } else {
-        Serial.println("Product not  found!");
-      }
-      
+    void removeProductFromScan() {
+      char eanCode[EAN_LENGTH];
+      scanEAN(eanCode, EAN_LENGTH);
+      removeProduct(eanCode);
     }
 
-    // Searches for a product in the inventory
-    int searchInventory(const char* obj) {
-        for(int i = 0; i < uniqueProducts; i++) {
-          if(strncmp(inventory[i], obj, EAN_LENGTH) == 0)
-            return i;
-        }
-        return -1;      // Product not found
-    }
-
-    void displayInventory() {
-      Serial.println("Inventory:");
-      for(int i = 0; i < uniqueProducts; i++) {
-        Serial.print("EAN: ");
-        Serial.print(inventory[i]);
-        Serial.print(", Quantity: ");
-        Serial.println(inventoryQuantity[i]);
-      }
-    }
 };
 
 #endif
